859c526984a7d6486261bfa483fb2b61742816d1
Gtp87 <fabio.giannitrapani@gmail.com>
2022-01-10 15:19:09 +0100

    end

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
-const app = new Vue({
-    el: "#app",
-    data: {},
+// Attraverso l’apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+//     Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati
 
-    created() {},
-    
-    methods: {}
-});
+const app = new Vue(
+    {
+        el: '#app',
+        data: {
+            emails: []
+        },
+        created() {
+            this.createMail();
+        },
+        methods: {
+            createMail: function () {
+                for (let i = 0; i < 10; i++) {
+                    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                        .then((response) => {
+                            this.emails.push(response.data.response);
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                        })
+                }
+            }
+        },
+    }
+)
